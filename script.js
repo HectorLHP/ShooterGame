c2ed72de9f013d4ce244956c0323d5934d913329
Hector <rankecillo@gmail.com>
2024-10-09 11:57:58 -0600

    Added new enemy worth more points with its own sound and score

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ ctx.font = '50px Impact';
 
 let timeToNextRaven = 0;
 let ravenInterval = 500;
+let timeToNextBat = 0; // Separate timer for bats
+let batInterval = 2000; // Time interval for spawning bats (e.g., every 2 seconds)
 let lastTime = 0;
 
 let ravens = [];
@@ -71,6 +73,67 @@ class Raven {
     }
     if (this.x < 0 - this.width) {
       gameOver = true;
+    }
+  }
+
+  draw() {
+    collisionCtx.fillStyle = this.color;
+    collisionCtx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+let bats = [];
+
+class Bat {
+  constructor() {
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
+    this.sizeModifier = Math.random() * 0.5 + 0.4;
+    this.width = this.spriteWidth * 0.4;
+    this.height = this.spriteHeight * 0.4;
+    this.x = canvas.width;
+    this.y = Math.random() * (canvas.height - this.height);
+    this.directionX = 20; // Faster than ravens
+    this.directionY = Math.random() * 4;
+    this.markedForDeletion = false;
+    this.image = new Image();
+    this.image.src = './images/bats.png';
+    this.frame = 0;
+    this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 50 + 50; // Similar to ravens
+
+    this.randomColors = [
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+    ];
+    this.color = `rgb(${this.randomColors[0]},${this.randomColors[1]},${this.randomColors[2]})`;
+    this.hasTrail = Math.random() > 0.5; // Optional
+  }
+
+  update(deltaTime) {
+    if (this.y < 0 || this.y > canvas.height - this.height) {
+      this.directionY = this.directionY * -1;
+    }
+    this.x -= this.directionX;
+    this.y += this.directionY;
+    if (this.x < 0 - this.width) this.markedForDeletion = true;
+    this.timeSinceFlap += deltaTime;
+    if (this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
     }
   }
 
@@ -202,6 +265,19 @@ window.addEventListener('click', function (e) {
       console.log(explosions);
     }
   });
+  bats.forEach((object) => {
+    if (
+      object.randomColors[0] === pc[0] &&
+      object.randomColors[1] === pc[1] &&
+      object.randomColors[2] === pc[2]
+    ) {
+      object.markedForDeletion = true;
+      score += 5; // More points for bats
+      let batExplosion = new Explosion(object.x, object.y, object.width);
+      batExplosion.sound.src = './audio/bats.wav';
+      explosions.push(batExplosion);
+    }
+  });
 });
 
 function animate(timestamp) {
@@ -209,6 +285,8 @@ function animate(timestamp) {
   collisionCtx.clearRect(0, 0, canvas.width, canvas.height);
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
+
+  // Raven spawn
   timeToNextRaven += deltaTime;
   if (timeToNextRaven > ravenInterval) {
     ravens.push(new Raven());
@@ -217,12 +295,22 @@ function animate(timestamp) {
       return a.width - b.width;
     });
   }
+
+  timeToNextBat += deltaTime;
+  if (timeToNextBat > batInterval) {
+    bats.push(new Bat());
+    timeToNextBat = 0;
+  }
+
   drawScore();
-  [...particles, ...ravens, ...explosions].forEach((object) =>
+  [...particles, ...ravens, ...bats, ...explosions].forEach((object) =>
     object.update(deltaTime)
   );
-  [...particles, ...ravens, ...explosions].forEach((object) => object.draw());
+  [...particles, ...ravens, ...bats, ...explosions].forEach((object) =>
+    object.draw()
+  );
 
+  bats = bats.filter((object) => !object.markedForDeletion);
   particles = particles.filter((object) => !object.markedForDeletion);
   ravens = ravens.filter((object) => !object.markedForDeletion);
   explosions = explosions.filter((object) => !object.markedForDeletion);
