d97ac85172bf99d5c88a18e94b485548042d48cd
Hector <rankecillo@gmail.com>
2024-10-09 11:20:12 -0600

    Added particles trail

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ class Raven {
       ',' +
       this.randomColors[2] +
       ')';
+    this.hasTrail = Math.random() > 0.4;
   }
 
   update(deltaTime) {
@@ -62,6 +63,11 @@ class Raven {
       if (this.frame > this.maxFrame) this.frame = 0;
       else this.frame++;
       this.timeSinceFlap = 0;
+      if (this.hasTrail) {
+        for (let i = 0; i < 5; i++) {
+          particles.push(new Particle(this.x, this.y, this.width, this.color));
+        }
+      }
     }
     if (this.x < 0 - this.width) {
       gameOver = true;
@@ -86,6 +92,7 @@ class Raven {
 }
 
 let explosions = [];
+
 class Explosion {
   constructor(x, y, size) {
     this.image = new Image();
@@ -123,6 +130,35 @@ class Explosion {
       this.size,
       this.size
     );
+  }
+}
+
+let particles = [];
+
+class Particle {
+  constructor(x, y, size, color) {
+    this.size = size;
+    this.x = x + this.size / 2 + Math.random() * 50 - 25;
+    this.y = y + this.size / 3 + Math.random() * 50 - 25;
+    this.radius = (Math.random() * this.size) / 10;
+    this.maxRadius = Math.random() * 20 + 35;
+    this.markedForDeletion = false;
+    this.speedX = Math.random() * 1 + 0.5;
+    this.color = color;
+  }
+  update() {
+    this.x += this.speedX;
+    this.radius += 0.5;
+    if (this.radius > this.maxRadius - 5) this.markedForDeletion = true;
+  }
+  draw() {
+    ctx.save();
+    ctx.globalAlpha = 1 - this.radius / this.maxRadius;
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
   }
 }
 
@@ -182,8 +218,12 @@ function animate(timestamp) {
     });
   }
   drawScore();
-  [...ravens, ...explosions].forEach((object) => object.update(deltaTime));
-  [...ravens, ...explosions].forEach((object) => object.draw());
+  [...particles, ...ravens, ...explosions].forEach((object) =>
+    object.update(deltaTime)
+  );
+  [...particles, ...ravens, ...explosions].forEach((object) => object.draw());
+
+  particles = particles.filter((object) => !object.markedForDeletion);
   ravens = ravens.filter((object) => !object.markedForDeletion);
   explosions = explosions.filter((object) => !object.markedForDeletion);
 
