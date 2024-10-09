1bce1435907610533b8f177592111522325007c6
Hector <rankecillo@gmail.com>
2024-10-08 20:11:46 -0600

    Added explosion animation and audio, game over conditions and screen

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ collisionCanvas.width = window.innerWidth;
 collisionCanvas.height = window.innerHeight;
 
 let score = 0;
+let gameOver = false;
 ctx.font = '50px Impact';
 
 let timeToNextRaven = 0;
@@ -25,11 +26,11 @@ class Raven {
     this.height = this.spriteHeight * this.sizeModifier;
     this.x = canvas.width;
     this.y = Math.random() * (canvas.height - this.height);
-    this.directionX = Math.random() * 10 - 5;
+    this.directionX = Math.random() * 20 - 10;
     this.directionY = Math.random() * 5 - 2.5;
     this.markedForDeletion = false;
     this.image = new Image();
-    this.image.src = 'raven.png';
+    this.image.src = './images/raven.png';
     this.frame = 0;
     this.maxFrame = 4;
     this.timeSinceFlap = 0;
@@ -48,6 +49,7 @@ class Raven {
       this.randomColors[2] +
       ')';
   }
+
   update(deltaTime) {
     if (this.y < 0 || this.y > canvas.height - this.height) {
       this.directionY = this.directionY * -1;
@@ -61,7 +63,9 @@ class Raven {
       else this.frame++;
       this.timeSinceFlap = 0;
     }
+    if (this.x < 0 - this.width) gameOver = true;
   }
+
   draw() {
     collisionCtx.fillStyle = this.color;
     collisionCtx.fillRect(this.x, this.y, this.width, this.height);
@@ -79,11 +83,68 @@ class Raven {
   }
 }
 
+let explosions = [];
+class Explosion {
+  constructor(x, y, size) {
+    this.image = new Image();
+    this.image.src = './images/boom.png';
+    this.spriteWidth = 200;
+    this.spriteHeight = 179;
+    this.size = size;
+    this.x = x;
+    this.y = y;
+    this.frame = 0;
+    this.sound = new Audio();
+    this.sound.src = './audio/boom.wav';
+    this.timeSinceLastFrame = 0;
+    this.frameInterval = 200;
+    this.markedForDeletion = false;
+  }
+  update(deltaTime) {
+    if (this.frame === 0) this.sound.play();
+    this.timeSinceLastFrame += deltaTime;
+    if (this.timeSinceLastFrame > this.frameInterval) {
+      this.frame++;
+      this.timeSinceLastFrame = 0;
+      if (this.frame > 5) this.markedForDeletion = true;
+    }
+  }
+  draw() {
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y - this.size / 4,
+      this.size,
+      this.size
+    );
+  }
+}
+
 function drawScore() {
   ctx.fillStyle = 'black';
   ctx.fillText('Score: ' + score, 50, 75);
   ctx.fillStyle = 'white';
   ctx.fillText('Score: ' + score, 55, 80);
+}
+
+function drawGameOver() {
+  ctx.textAlign = 'center';
+  ctx.fillStyle = 'black';
+  ctx.fillText(
+    'GAME OVER, your score is ' + score,
+    canvas.width / 2,
+    canvas.height / 2
+  );
+  ctx.fillStyle = 'white';
+  ctx.fillText(
+    'GAME OVER, your score is ' + score,
+    canvas.width / 2,
+    canvas.height / 2 + 5
+  );
 }
 
 window.addEventListener('click', function (e) {
@@ -96,8 +157,11 @@ window.addEventListener('click', function (e) {
       object.randomColors[1] === pc[1] &&
       object.randomColors[2] === pc[2]
     ) {
+      // Collision detected by color
       object.markedForDeletion = true;
       score++;
+      explosions.push(new Explosion(object.x, object.y, object.width));
+      console.log(explosions);
     }
   });
 });
@@ -116,10 +180,13 @@ function animate(timestamp) {
     });
   }
   drawScore();
-  [...ravens].forEach((object) => object.update(deltaTime));
-  [...ravens].forEach((object) => object.draw());
+  [...ravens, ...explosions].forEach((object) => object.update(deltaTime));
+  [...ravens, ...explosions].forEach((object) => object.draw());
   ravens = ravens.filter((object) => !object.markedForDeletion);
-  requestAnimationFrame(animate);
+  explosions = explosions.filter((object) => !object.markedForDeletion);
+
+  if (!gameOver) requestAnimationFrame(animate);
+  else drawGameOver();
 }
 
 animate(0);
